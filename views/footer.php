178332d53c923393fd9dbf9b4b8d655285aabe35
0xCronos <56097041+0xCronos@users.noreply.github.com>
2020-04-08 04:13:14 -0400

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,64 +1,37 @@
 <!-- Footer -->
 <footer class="page-footer font-small">
-
-  <!-- Footer Links -->
   <div class="container">
-
-    <!-- Grid row-->
     <div class="row text-center d-flex justify-content-center pt-5 mb-3">
-
-      <!-- Grid column -->
       <div class="col-md-2 mb-3">
         <h6 class="text-uppercase font-weight-bold ">
           <a class="text-white" href="#">Inicio</a>
         </h6>
       </div>
-      <!-- Grid column -->
-
-      <!-- Grid column -->
       <div class="col-md-2 mb-3">
         <h6 class="text-uppercase font-weight-bold">
           <a class="text-white" href="#community">Comunidad</a>
         </h6>
       </div>
-      <!-- Grid column -->
-
-      <!-- Grid column -->
       <div class="col-md-2 mb-3">
         <h6 class="text-uppercase font-weight-bold">
           <a class="text-white" href="#discord">Contacto</a>
         </h6>
       </div>
-      <!-- Grid column -->
-
     </div>
-    <!-- Grid row-->
     <hr class="rgba-white-light" style="margin: 0 15%;">
 
-    <!-- Grid row-->
     <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
-
-      <!-- Grid column -->
       <div class="col-md-8 col-12 mt-5">
         <p style="line-height: 1.7rem">
             Creado por Toroneko's Co. para la comunidad Chillin con todo el amor
             del mundo.
         </p>
       </div>
-      <!-- Grid column -->
-
     </div>
-    <!-- Grid row-->
     <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
-
-    <!-- Grid row-->
     <div class="row pb-3">
-
-      <!-- Grid column -->
       <div class="col-md-12">
-
         <div class="mb-5 text-center">
-
           <!-- Facebook -->
           <a class="fb-ic text-decoration-none text-white" href="https://discord.gg/9HXaKGA" target=_blank>
             <i class="fab fa-facebook-f fa-lg white-text mr-4"> </i>
@@ -75,18 +48,10 @@
           <a class="ins-ic text-decoration-none text-white" href="https://discord.gg/9HXaKGA" target=_blank>
             <i class="fab fa-discord fa-lg white-text mr-4"> </i>
           </a>
-
         </div>
-
       </div>
-      <!-- Grid column -->
-
     </div>
-    <!-- Grid row-->
-
   </div>
-  <!-- Footer Links -->
-
   <!-- Copyright -->
   <div class="footer-copyright text-center py-3">© 2020 Copyright:
     <a class="text-white" href="#"> Chillin</a>
@@ -94,4 +59,3 @@
   <!-- Copyright -->
 
 </footer>
-<!-- Footer -->
